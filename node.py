65bff5fe68eebc83fa7ddf83deff52a6551f9ace
Michelle Li <michelleyli123@gmail.com>
2023-05-17 16:25:15 -0400

    edit gui

--- node.py
+++ node.py
@@ -1,9 +1,9 @@
 from copy import deepcopy
+from board import Board
 
-class Node:
-    def __init__(self, board, move=None, val=None):
+class State:
+    def __init__(self, board, move=None):
         self.board = board
-        self.val = val
         self.move = move
 
     def get_children(self, max_player):
@@ -12,19 +12,16 @@ class Node:
         children = []
 
         if max_player:
-            avail_moves = Checkers.possible_moves(curr)
+            avail_moves = Board.possible_moves(curr)
         else:
-            avail_moves = Checkers.possible_moves(curr, False)
+            avail_moves = Board.possible_moves(curr, False)
         for i in range(len(avail_moves)):
             current_pos = avail_moves[i][0]
             new_pos = avail_moves[i][1]
             state = deepcopy(curr)
-            Checkers.move_piece(state, current_pos, new_pos)
-            children.append(Node(state, [current_pos, new_pos]))
+            Board.move_piece(state, current_pos, new_pos)
+            children.append(State(state, [current_pos, new_pos]))
         return children
-
-    def set_value(self, val):
-        self.val = val
 
     def get_board(self):
         return self.board
